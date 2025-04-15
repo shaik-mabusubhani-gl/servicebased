30732615ad17de4fb621a55ab6fd2fe26958d74b
podilithanoj <101394210+podilithanoj@users.noreply.github.com>
2025-04-15 20:28:30 +0530

    Delete terraform/aws123.network/modules/glue/modules/glue directory
    rem

--- terraform/aws123.network/modules/glue/modules/glue/main.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_glue_catalog_database" "default" {
-  name = "${var.env}_glue_db"
-}
--- terraform/aws123.network/modules/glue/modules/glue/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "glue_database_name" {
-  value = aws_glue_catalog_database.default.name
-}
