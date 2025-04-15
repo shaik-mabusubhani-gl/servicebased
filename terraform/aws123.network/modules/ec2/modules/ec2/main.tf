f9fc571ac0f613c919b1078ff612ccb8e16e2b19
podilithanoj <101394210+podilithanoj@users.noreply.github.com>
2025-04-15 20:28:13 +0530

    Delete terraform/aws123.network/modules/ec2/modules/ec2 directory
    not req

--- terraform/aws123.network/modules/ec2/modules/ec2/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = var.subnet_id
-  tags = {
-    Name = "${var.env}-ec2"
-  }
-}
