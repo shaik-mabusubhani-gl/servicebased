05bdad9c2ced85115830cd581a1f737b79a6bcd7
shaik-mabusubhani-gl <shaik.mabusubhani@globallogic.com>
2025-04-16 15:47:39 +0530

    removed ec2 folder

--- terraform/aws123.app/java-app-service-1/envs/prod/ec2/main.tf
+++
@@ -1,7 +0,0 @@
-module "ec2" {
-  source         = "../../../services/ec2/modules/ec2"
-  env            = var.env
-  ami_id         = var.ami_id
-  instance_type  = var.instance_type
-  subnet_id      = var.subnet_id
-}
