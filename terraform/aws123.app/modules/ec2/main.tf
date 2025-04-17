a87940273db40b7d751b144e76531031ea35a1ef
shaik-mabusubhani-gl <shaik.mabusubhani@globallogic.com>
2025-04-17 12:56:33 +0530

    Added the new file

--- terraform/aws123.app/modules/ec2/main.tf
+++ terraform/aws123.app/modules/ec2/main.tf
@@ -1,3 +1,18 @@
+provider "aws" {
+  region = var.region
+  profile = "default"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "tf-state-dksoni"
+    region = "us-west-2"
+    key    = "dev/terraform.tfstate"
+    profile = "default"
+  }
+}
+
+
 resource "aws_instance" "demo_instance" {
 
   ami               = var.ami_id
